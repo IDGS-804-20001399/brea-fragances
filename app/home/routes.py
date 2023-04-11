e242dfc241870af26e72f487dead4f4109b87d1c
EdgarPureco <pureco.edgar.1am@gmail.com>
2023-04-10 21:59:08 -0600

    Display products

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,15 +1,17 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_security import login_required, current_user, roles_required, roles_accepted
-
+from app.product.models import Product
 home = Blueprint('home', __name__,
                  template_folder='templates')
             
 @home.route('/')
 def index():
-    return render_template('home.html', title='Brea Fragances - Home')
+    products = Product.query.all()
+    return render_template('home.html', title='Brea Fragances - Home', products=products)
 
 @home.route('/products')
 @login_required
 @roles_accepted('customer', 'admin')
 def products():
-    return render_template('allProducts.html', title='Products')
+    products = Product.query.all()
+    return render_template('allProducts.html', title='Products', products=products)
